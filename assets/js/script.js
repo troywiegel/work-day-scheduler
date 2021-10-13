27cf6468efcbc4eff18dbaa48705f87dfe3bbabf
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-10-12 22:27:43 -0700

    added notes to js page and added some initial html

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// add entry to calendar and it saves it to local storage
+//label entries by time of day
+
+//one part want to be able to type in box and click save so it goes to local storage
+//two part each time the page is refreshed, entries need to be pulled from local storage and added to page
+//three part change the class names in text areas based on what hour it is
+
+// use textarea for entries
+//one div per entry with a p tag, textarea and a button at the end
+
+//style isn't super important but functionality is
+
+// use bootstrap grid to setup? maybe get rid of css file or elements? make time and button take up a little amount of space and the text area big
+
+// colors don't need to update in real time just when page is refreshed
+
+// want one onclick to work for every saveBtn don't want a separate one for each
+//use class names not ID for the onlick part
+// see office hours for tuesday on how to setup onclick
