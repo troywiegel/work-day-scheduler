16441b18f0734dd7fa58e521fee199a435f0b20e
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-10-14 17:18:46 -0700

    created timeTracking and getStorage functions to complete the scheduler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,6 @@
-// add entry to calendar and it saves it to local storage
-//label entries by time of day
+var currentDay = moment().format('MMMM Do YYYY, h:mm a');
+$('#currentDay').html(currentDay);
 
-//one part want to be able to type in box and click save so it goes to local storage
-//two part each time the page is refreshed, entries need to be pulled from local storage and added to page
-//three part change the class names in text areas based on what hour it is
-
-// use textarea for entries
-//one div per entry with a p tag, textarea and a button at the end
-
-//style isn't super important but functionality is
-
-// use bootstrap grid to setup? maybe get rid of css file or elements? make time and button take up a little amount of space and the text area big
-
-// colors don't need to update in real time just when page is refreshed
-
-// want one onclick to work for every saveBtn don't want a separate one for each
-//use class names not ID for the onlick part
-// see office hours for tuesday on how to setup onclick
-
-// console.log('moment', moment(), hour())
-
-// maybe make an array and set var names to an int to compare with moment time??
-
-var time = moment().format('hh');
-var nine = $('#9')
 
 $('.saveBtn').on('click', function() {
 
@@ -31,9 +8,47 @@ $('.saveBtn').on('click', function() {
 
 });
 
-if (time > (nine)) {
+function timeTracking() {
 
-    $(nine).addClass('past')
-    console.log('getting assigned class?', nine);
+    $('.time-block').each(function() {
 
-}
+        var currentTime = moment().hour();
+        var calendarTime = parseInt($(this).attr('id').split('hour')[1]);
+
+        if (calendarTime < currentTime) {
+
+            $(this).addClass('past');
+            $(this).removeClass('present');
+            $(this).removeClass('future');
+            
+        } else if (calendarTime === currentTime) {
+
+            $(this).removeClass('past');
+            $(this).addClass('present');
+            $(this).removeClass('future');
+
+        } else {
+
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+
+        }
+    })
+};
+
+function getStorage() {
+
+$('#hour9 .description').val(localStorage.getItem('9'));
+$('#hour10 .description').val(localStorage.getItem('10'));
+$('#hour11 .description').val(localStorage.getItem('11'));
+$('#hour12 .description').val(localStorage.getItem('12'));
+$('#hour13 .description').val(localStorage.getItem('13'));
+$('#hour14 .description').val(localStorage.getItem('14'));
+$('#hour15 .description').val(localStorage.getItem('15'));
+$('#hour16 .description').val(localStorage.getItem('16'));
+$('#hour17 .description').val(localStorage.getItem('17'));
+
+};
+getStorage();
+timeTracking();
